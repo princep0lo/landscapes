1f5bd50617f9dd94051db9b0e24ecb7efd8fc24c
Michal Gornicki <michal.gornicki@ithouse.co>
2023-02-07 17:34:16 +0100

    Updates

---
+++ precache-manifest.9607bf8e72d2df85452759ed9b1e7392.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faedb3589b9543cd306cd8cd2960f6e0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f7e5c3d6ef4c8f1b7cfd",
+    "url": "./static/css/main.81c22762.chunk.css"
+  },
+  {
+    "revision": "9d3368280aeb91f90fa2",
+    "url": "./static/js/2.e1917bf5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e1917bf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7e5c3d6ef4c8f1b7cfd",
+    "url": "./static/js/main.dd41eefb.chunk.js"
+  },
+  {
+    "revision": "0f993b0e695a7ed7022f",
+    "url": "./static/js/runtime-main.2544a7cf.js"
+  }
+]);
